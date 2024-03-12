8c0fb9b9a364649cf76afb84c3406e9047d898e7
zhirain <154818728+kfcvo@users.noreply.github.com>
2024-03-12 21:26:36 +0800

    Add files via upload

---
+++ text5/script.js
@@ -0,0 +1,12 @@
+document.write("Hello, world!");
+function sayhello(){
+  var re=prompt ("what is your name");
+  alert("Hello"+re+",have a nice day!");
+};
+//sayhello();
+
+function showtopic(){
+  var x =document.getElementById('demo');
+  x.style.fontSize="25px";
+  x.style.color="red";
+}
